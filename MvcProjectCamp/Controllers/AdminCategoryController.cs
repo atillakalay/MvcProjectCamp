b29a294ede1380141bcded0765578f9769871698
atilla kalay <atilla.kalayy@gmail.com>
2021-05-07 12:55:36 +0300

    Theme added

---
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var result = _categoryManager.GetAll();
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                _categoryManager.Add(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+    }
+}
